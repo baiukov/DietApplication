76b19800d9bbdea972b77d0393b1a7caf8d7b093
andreukuznec <kuza13@vse.cz>
2024-01-24 00:21:04 +0100

    final version

--- dietApplication_frontend/dev/dietPlan/dietPlan.controller.ts
+++ dietApplication_frontend/dev/dietPlan/dietPlan.controller.ts
@@ -1,22 +1,21 @@
-import { AppService } from '../app.service'
-import { Events } from '../enums/Events.enum'
-import { ServerEvents } from '../enums/ServerEvents.enum'
-import { DietPlanService } from './dietPlan.service'
+import { AppService } from '../app.service';
+import { Events } from '../enums/Events.enum';
+import { ServerEvents } from '../enums/ServerEvents.enum';
+import { DietPlanService } from './dietPlan.service';
 
 export class DietPlanController {
-
 	private dietPlanService: DietPlanService;
-	
+
 	constructor(dietPlanService: DietPlanService) {
 		this.dietPlanService = dietPlanService;
 
 		AppService.on(Events.GetPlans, (userID: string) => {
-			this.dietPlanService.getPlans(userID)
-		})
+			this.dietPlanService.getPlans(userID);
+		});
 
-		AppService.onServer(ServerEvents.GetPlans, (plan: Array<Array<Record<string, string>>>) => {
-			this.dietPlanService.setPlans(plan)
-		})
+		AppService.onServer(ServerEvents.GetPlans, (plan: any) => {
+			console.log('Plan controller:', plan);
+			this.dietPlanService.setPlans(plan);
+		});
 	}
-
 }
--- dietApplication_frontend/dev/dietPlan/dietPlan.service.ts
+++ dietApplication_frontend/dev/dietPlan/dietPlan.service.ts
@@ -1,58 +1,43 @@
-import { AppService } from '../app.service'
-import { ServerEvents } from '../enums/ServerEvents.enum'
+import { AppService } from '../app.service';
+import { ServerEvents } from '../enums/ServerEvents.enum';
 
 export class DietPlanService {
+	constructor() {
+		this.getPlans(sessionStorage.getItem('userID') as string);
+	}
 
 	public getPlans = (userID: string) => {
-		AppService.emitServer(ServerEvents.GetPlans, [userID])
-	}
+		AppService.emitServer(ServerEvents.GetPlans, [userID]);
+	};
 
-	public setPlans = (plans: Array<Array<Record<string, string>>>) => {
+	public setPlans = (plansStr: string) => {
+		console.log('Plans: ' + plansStr);
+		const container = $('.plans');
+		container.empty(); // Clear existing content
+		const plans = JSON.parse(plansStr);
+		let dayIndex = 0;
+		plans.forEach((currentDay: any) => {
+			console.log(currentDay);
+			const dayArticle = $('<article>').addClass('day-plan');
+			const title = $(`<h3>`)
+				.addClass('day-title')
+				.text(`Day ${dayIndex + 1}`);
+			dayIndex++;
+			dayArticle.append(title);
+			currentDay.forEach((dayObject: any) => {
+				Object.entries(dayObject).forEach(([key, value]) => {
+					const section = $('<section>').addClass('meal').attr('id', key);
+					const subtitle = $('<h4>').text(key);
+					const ul = $('<ul>');
+					const li = $('<li>').text(value as string);
 
-		const container = $(".plans")
+					ul.append(li);
+					section.append(subtitle, ul);
+					dayArticle.append(section);
+				});
 
-		for (let i = 0; i < plans.length; i++) {
-
-			const currentDay = plans[i];
-
-			const article = document.createElement("article")
-			$(article).addClass("day-plan")	
-	
-			const title = document.createElement("H3")
-			$(title).addClass("day-title")
-				.text("Day " + (i + 1))
-
-			$(article).append(title)	
-
-			for (let j = 0; j < currentDay.length; j++) {
-
-				const currentCourse = currentDay[j]
-				const courseName = currentCourse.key;
-				const food = currentCourse.value;
-
-				const section = document.createElement("section")
-				$(section).addClass("meal")
-				.attr("id", courseName)
-
-				const subtitile = document.createElement("h4")
-				$(subtitile).text(courseName)
-				
-				const ul = document.createElement("ul")
-				const li = document.createElement("li")
-				$(li).text(food)
-				$(ul).append(li)
-				
-				$(section).append(subtitile, ul)
-
-				$(article).append(section)
-
-			}
-
-			$(container).append(article)
-		}
-	}
-
-	
-
-
+				container.append(dayArticle);
+			});
+		});
+	};
 }
